e854fb24573ef6c3327ca16d900504068dd768e0
Franz Gusto <franz@rungalileo.io>
2022-12-27 00:07:26 +0100

    added localisation

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,49 +1,44 @@
-import { ImageUploader, Button, ImageUploadItem, Toast } from "antd-mobile";
+import {
+  ImageUploader,
+  Button,
+  ImageUploadItem,
+  Toast,
+  ConfigProvider,
+} from "antd-mobile";
 import { prepareFileList } from "../utils/files";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AddCircleOutline } from "antd-mobile-icons";
 import { useSpring, animated } from "react-spring";
-//shakediv takes in html elements and returns a div that shakes when clicked
-const ShakeDiv = ({ children, degree, stop }: any) => {
+import { enUS } from "../localisation/en_Us";
+
+const useShake = (degree: number) => {
   const [props, set] = useSpring<any>(() => ({ transform: "rotate(0deg)" }));
-  const [intervalId, setIntervalId] = useState<NodeJS.Timer>();
+  const intervalId = useRef<NodeJS.Timer>();
 
   const interval = 200;
 
   useEffect(() => {
-    if (stop) {
-      handleStop();
-    }
-  }, [stop]);
-
-  useEffect(() => {
-    if (!stop) {
-      const intervalId = setInterval(() => {
-        set({
-          transform: `rotate(${degree}deg)`,
-          width: "15px",
-        });
-        setTimeout(() => {
-          set({ transform: `rotate(-${degree}deg)`, width: "25px" });
-        }, interval / 2);
-      }, interval);
-      setIntervalId(intervalId);
-      return () => clearInterval(intervalId);
-    }
+    const id = setInterval(() => {
+      set({
+        transform: `rotate(${degree}deg)`,
+        width: "15px",
+      });
+      setTimeout(() => {
+        set({ transform: `rotate(-${degree}deg)`, width: "25px" });
+      }, interval / 2);
+    }, interval);
+    intervalId.current = id;
+    return () => clearInterval(id);
   }, []);
 
-  const handleStop = () => {
-    clearInterval(intervalId);
-    setTimeout(() => {
-      set({ transform: `rotate(0deg)` });
-    }, interval);
+  const stopAnimation = () => {
+    // clearInterval(intervalId.current);
+    // setTimeout(() => {
+    //   set({ transform: `rotate(0deg)` });
+    // }, interval);
   };
 
-  return (
-    <animated.div style={props} onClick={handleStop}>
-      {children}
-    </animated.div>
-  );
+  return [props, stopAnimation] as any;
 };
 
 export const defaultImages: Array<ImageUploadItem & { uid: string }> = [
@@ -74,56 +69,81 @@ export const ImageUpload = ({
       setOpen(true);
     }
   }, [fileList]);
+
+  const [animation, stopAnimation] = useShake(9);
+
+  useEffect(() => {
+    if (!showModelAtFirst && fileList.length >= minCount) {
+      stopAnimation();
+    }
+  }, [showModelAtFirst, stopAnimation]);
   return (
     <div style={{ width: "100%" }}>
       <p>
         Transform your pet into a work of art with 5 images and AI magic. Upload
         5 images of your pet and get the best images the next day.
       </p>
-      <ImageUploader
-        // beforeUpload={(file, files) => {
-        //   // const validFile = beforeUploadValidation(file.size);
-        //   if (file) {
-        //     return file;
-        //   } else {
-        //     Toast.show("File size must be less than 25MB");
-        //     return null;
-        //   }
-        // }}
-        value={fileList}
-        style={{ "--cell-size": "150px" }}
-        onChange={(items: ImageUploadItem[]) => {
-          setFileList(items.filter((item) => (item as any)?.uid !== "-1"));
-        }}
-        upload={prepareFileList}
-        multiple
-        maxCount={maxCount}
-        showUpload={fileList.length < maxCount}
-        onCountExceed={(exceed) => {
-          Toast.show(
-            `Photo count must be between ${minCount} and ${maxCount}. ${exceed}`
-          );
-        }}
-      >
-        <ShakeDiv stop={!showModelAtFirst} degree={5}>
-          <div
-            style={{
-              width: "150px",
-              height: "150px",
-              borderRadius: 40,
-              backgroundColor: "#f5f5f5",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              color: "#999999",
-            }}
-          >
-            <ShakeDiv stop={!showModelAtFirst} degree={8}>
+      <ConfigProvider locale={enUS}>
+        <ImageUploader
+          // beforeUpload={(file, files) => {
+          //   // const validFile = beforeUploadValidation(file.size);
+          //   if (file) {
+          //     return file;
+          //   } else {
+          //     Toast.show("File size must be less than 25MB");
+          //     return null;
+          //   }
+          // }}
+          value={fileList}
+          style={{ "--cell-size": "150px" }}
+          onChange={(items: ImageUploadItem[]) => {
+            setFileList(items.filter((item) => (item as any)?.uid !== "-1"));
+          }}
+          upload={prepareFileList}
+          multiple
+          maxCount={maxCount}
+          showUpload={fileList.length < maxCount}
+          onCountExceed={(exceed) => {
+            Toast.show(
+              `Photo count must be between ${minCount} and ${maxCount}. ${exceed}`
+            );
+          }}
+        >
+          {showModelAtFirst ? (
+            <animated.div key={"1212"} style={animation}>
+              <div
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  borderRadius: 40,
+                  backgroundColor: "#f5f5f5",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: "#999999",
+                }}
+              >
+                <AddCircleOutline style={{ fontSize: 32 }} />
+              </div>
+            </animated.div>
+          ) : (
+            <div
+              style={{
+                width: "150px",
+                height: "150px",
+                borderRadius: 40,
+                backgroundColor: "#f5f5f5",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "#999999",
+              }}
+            >
               <AddCircleOutline style={{ fontSize: 32 }} />
-            </ShakeDiv>
-          </div>
-        </ShakeDiv>
-      </ImageUploader>
+            </div>
+          )}
+        </ImageUploader>
+      </ConfigProvider>
       <br />
       {fileList.length > 2 && (
         <Button
